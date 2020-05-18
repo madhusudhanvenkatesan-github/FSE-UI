3beeee49d10d80e33874a2cdfa7bb5f40b636ece
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-18 21:07:25 +0530

    Created components Home and mulitselection under project and utilities

---
+++ FSE-UI/ClientApp/src/components/project/Home.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Home extends Component {
+    static displayName = Home.nam;
+
+    render() {
+        return (
+            <div>
+                <ProjectManagment />
+            </div>
+        );
+    }
+}
+export default Home;
