98f0cca21b4568bebed74b8dc240705dfceef06b
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-19 20:31:07 +0530

    Added Home.js

---
+++ FSE-UI/ClientApp/src/components/user/Home.js
@@ -0,0 +1,17 @@
+﻿import React, { Component } from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import UserManagement from './Managements';
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <UserManagement />
+
+            </div>
+        );
+    }
+
+}
+export default Home;
