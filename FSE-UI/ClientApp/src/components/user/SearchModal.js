6d7a1755077baab927125380e3c1e6c55a031444
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-19 20:45:02 +0530

    added Searchmodal

---
+++ FSE-UI/ClientApp/src/components/user/SearchModal.js
@@ -0,0 +1,30 @@
+﻿import React, { Component, Fragment } from 'react';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
+import UserSearch from './Search';
+export class SearchModal extends Component {
+    static displayName = SearchModal.name;
+    state = {
+        modal: false
+    }
+    toggle = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }));
+    }
+    render() {
+        return <Fragment>
+            <Button
+                color="info"
+                onClick={this.toggle}
+                style={{ minWidth: "100px" }}>Search User</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ minWidth: '500px' }}>
+                <ModalHeader toggle={this.toggle}>User Search</ModalHeader>
+                <ModalBody>
+                    <UserSearch onSelect={this.props.onSelect} onToggle={this.toggle}>
+                    </UserSearch>
+                </ModalBody>
+            </Modal>
+        </Fragment>
+    }
+}
+export default SearchModal
