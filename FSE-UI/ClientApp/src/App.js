48e7038ad5edc557e4643c1be894427580816002
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-19 21:00:12 +0530

    updated app.js

--- FSE-UI/ClientApp/src/App.js
+++ FSE-UI/ClientApp/src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
 import './custom.css'
+import AppHeader from './components/AppHeader';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <AppHeader />
+                <Home />
+            </Fragment>
+        );
+    }
 }
