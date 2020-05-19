a099f2a535c836c7a177ee19d1d177a2af61f063
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-19 22:03:14 +0530

    updated managment

--- FSE-UI/ClientApp/src/components/project/Home.js
+++ FSE-UI/ClientApp/src/components/project/Home.js
@@ -1,12 +1,13 @@
 ﻿import React, { Component } from 'react';
+import { Management } from './Management';
 
 export class Home extends Component {
-    static displayName = Home.nam;
+    static displayName = Home.name;
 
     render() {
         return (
             <div>
-                <ProjectManagment />
+                <Management />
             </div>
         );
     }
