528b900d77797fda1346abbddd8e5d41d28f1d81
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-19 21:19:38 +0530

    Added AppHeader.js

---
+++ FSE-UI/ClientApp/src/components/AppHeader.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+} from 'reactstrap';
+class AppHeader extends Component {
+
+    render() {
+        return <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/">
+                <p style={{ color: "white" }}> Project Management</p>
+            </NavbarBrand>
+        </Navbar>;
+    }
+}
+export default AppHeader; 
