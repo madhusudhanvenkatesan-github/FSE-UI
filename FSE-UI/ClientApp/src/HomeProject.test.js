a9d5f34edad928c04602773ed95dec9e96b85540
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-20 23:04:53 +0530

    added home test case

---
+++ FSE-UI/ClientApp/src/HomeProject.test.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/project/Home';
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Home />
+        , div);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+});
