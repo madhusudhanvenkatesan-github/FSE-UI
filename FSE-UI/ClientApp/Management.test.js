7826bccf6c657701a64d6d73d22556cda6900e73
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-20 23:09:04 +0530

    updated test cases

---
+++ FSE-UI/ClientApp/Management.test.js
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import Management from './components/project/Management';
+
+Enzyme.configure({ adapter: new Adapter() });
+beforeAll(() => {
+    global.fetch = jest.fn();
+});
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<ProjectManagement />, { disableLifecycleMethods: true });
+});
+afterEach(() => {
+    wrapper.unmount();
+});
+it("check if the component is loaded without crashing",
+    (done) => {
+        const spyDidMount = jest.spyOn(Management.prototype, "componentDidMount");
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                status: 200,
+                json: () => {
+                    return Promise.resolve([{
+                        projId: "P/1",
+                        projectTitle: "Project test",
+                        startDate: "2020-04-30",
+                        endDate: "2020-05-03",
+                        pmUsrId: "Usr/1",
+                        pmUsrName: "Test User",
+                        totalTaskCount: 3,
+                        completedTaskCount: 1,
+                        priority: 1
+
+                    }]);
+                }
+            });
+        });
+        const didMount = wrapper.instance().componentDidMount();
+
+        expect(spyDidMount).toHaveBeenCalled();
+        done();
+    })
