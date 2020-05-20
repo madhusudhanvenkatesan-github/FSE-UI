22a709bf203505517653834493b58564f6ddf114
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-20 23:24:17 +0530

    updated test case for search task

---
+++ FSE-UI/ClientApp/src/SearchTask.test.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import SearchTask from './components/task/SearchTask';
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    function getTaskItemForMod() {
+        return '';
+    }
+    ReactDOM.render(
+        <SearchTask onGetParam={getTaskItemForMod} />
+        , div);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+});
