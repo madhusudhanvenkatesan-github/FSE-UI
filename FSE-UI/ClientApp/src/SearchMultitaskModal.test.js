410bf53f3e850083bef790f6f9cccdb9ff8ea21c
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-20 23:26:36 +0530

    Updated searchmodal test cases

---
+++ FSE-UI/ClientApp/src/SearchMultitaskModal.test.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchMultitaskModal from './components/Task/SearchMultitaskModal';
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <SearchMultitaskModal />
+        , div);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+});
