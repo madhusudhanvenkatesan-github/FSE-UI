d78935e27f4dea7ae8ff8b675304cb3ae547dc09
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-20 23:16:21 +0530

    updated listtask test cases

---
+++ FSE-UI/ClientApp/src/components/ListTask.test.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import ListTask from './components/task/ListTask';
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <ListTask />
+        , div);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+});
